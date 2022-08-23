eea9bbf8955a35f8e185d81026468b465fb64b7c
OrangeDev2 <47803678+OrangeDev2@users.noreply.github.com>
2022-08-23 00:35:40 -0700

    Update Budget App.py

--- Budget App.py
+++ Budget App.py
@@ -1,41 +1,52 @@
-class Category:
+import json
 
-  ledger = []
+class Category():
+
+  ledger_list = []
   
-  #default constructor
-  #def __init__(self):
-  #  self.ledger = ledger
-
-  def deposit(self, amount, description):
-    self.amount = amount
-    self.description = description
-
-    print('amount is...', self.amount)
-    print('desicrioptn is...', description)
-
-  def withdraw(self, amount, description):
-    self.amount = amount
-    self.description = description
-
-  def get_balance(self):
-    # balance -> deposit-withdraw
-    print('asf')
-
-  def transfer(self, amount, anotherBudgetCategory):
-    self.amount = amount
-    self.anotherBudgetCategory = anotherBudgetCategory
-
-  def check_funds(self, amount):
-    self.amount = amount
-
+  def __init__(self, category = str()):
+    self.category = category
+    title = '*'*int((30-len(category))/2) + category + '*'*int((30-len(category))/2)
+    print(title)
     
+  def __str__(self):
+    return self.category
+    
+  def deposit(self, amount = int(), description = str()):
+    Category.ledger_list.append(f'{{"amount": {amount}, "description": "{description}"}}')
+    
+    new_description = json.loads(Category.ledger_list[0])['description'][0:23]
+    new_amount = str(json.loads(Category.ledger_list[0])['amount'])[0:7]
+    space = ' ' * (30 - len(new_description) - len(new_amount))
+    
+    print(new_description + space + new_amount)
+    
+    Category.ledger_list.remove(f'{{"amount": {amount}, "description": "{description}"}}')
+      
+
+  def withdraw(self, amount = int(), description = str()):
+    Category.ledger_list.append(f'{{"amount": {-1 * amount}, "description": "{description}"}}')
+
+    new_description = json.loads(Category.ledger_list[0])['description'][0:23]
+    new_amount = str(json.loads(Category.ledger_list[0])['amount'])[0:7]
+    space = ' ' * (30 - len(new_description) - len(new_amount))
+    
+    print(new_description + space + new_amount)
+    
+    Category.ledger_list.remove(f'{{"amount": {-1 * amount}, "description": "{description}"}}')
+    
+  def get_balance(self):
+    print('')
+
+  def transfer(self, amount = int(), categoryTransfer = str()):
+    print('')
+
+  def check_funds(self, amount = int()):
+    print('')
+
+
 def create_spend_chart(categories):
-  # categories -> [food, clothing, auto]
-  print('test', categories)
-
-
-# create object of the class
-obj = Category()
-
-# calling the instance method using the object obj
-print(obj.deposit(58198, 'something'))
+  pass
+  #print(categories)
+  #for i in Category.ledger_list:
+  #  print(json.loads(i)['description'])
